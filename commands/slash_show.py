76646aa9838c938e6a213961353a97dddf7efbb8
nyamb0t <nrnm-a@yahoo.co.jp>
2025-04-10 23:22:57 +0900

    slash_show.py を更新

--- commands/slash_show.py
+++ commands/slash_show.py
@@ -12,19 +12,19 @@ async def shownumber(interaction: discord.Interaction):
     # VCの設定確認
     vc_channel_id = data.get("vc_channel")
     vc_channel = interaction.guild.get_channel(vc_channel_id) if vc_channel_id else None
-    vc_info = f"<#{vc_channel.id}>（‪‪❤︎‬ {vc_channel.name}）" if vc_channel else "　まだ設定されてないよ〜！"
+    vc_info = f"\n<‪‪❤︎‬　#{vc_channel.id}>" if vc_channel else "　まだ設定されてないよ〜！"
 
     # テキストチャンネルの設定確認
     text_channel_ids = data.get("text_channels", [])
     if text_channel_ids:
-        text_info = "\n".join([f"‪‪❤︎‬ <#{ch_id}>" for ch_id in text_channel_ids])
+        text_info = "\n".join([f"‪‪❤︎‬　<#{ch_id}>" for ch_id in text_channel_ids])
     else:
         text_info = "　まだ設定されてないよ〜！"
 
     message = (
         f"**いまの設定**\n"
-        f"**【VC】**：{vc_info}\n"
-        f"**【転送チャンネル】**：\n{text_info}"
+        f"【VC】\n{vc_info}\n"
+        f"【転送チャンネル】\n{text_info}"
     )
     await interaction.response.send_message(message, ephemeral=True)
 
