b1971823263ff8c81775c4810c25583127036494
nyamb0t <nrnm-a@yahoo.co.jp>
2025-04-10 23:41:33 +0900

    slash_show.py を更新

--- commands/slash_show.py
+++ commands/slash_show.py
@@ -12,7 +12,7 @@ async def shownumber(interaction: discord.Interaction):
     # VCの設定確認
     vc_channel_id = data.get("vc_channel")
     vc_channel = interaction.guild.get_channel(vc_channel_id) if vc_channel_id else None
-    vc_info = f"　‪‪⋆　<‪‪#{vc_channel_id}>" if vc_channel else "　まだ設定されてないよ〜！"
+    vc_info = f"　‪‪⋆　{vc_channel.name}" if vc_channel else "　まだ設定されてないよ〜！"
 
     # テキストチャンネルの設定確認
     text_channel_ids = data.get("text_channels", [])
