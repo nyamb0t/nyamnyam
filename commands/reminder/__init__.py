7e74ec5ca5e9a43c92c8146de3669e036fb5aedf
nyamb0t <nrnm-a@yahoo.co.jp>
2025-04-11 01:40:25 +0900

    __init__.py を更新

--- commands/reminder/__init__.py
+++ commands/reminder/__init__.py
@@ -1,7 +1,8 @@
-# commands/render/__init__.py
-# --- render/daily.py のコマンドをBotに登録するためのファイル！
+# commands/reminder/__init__.py
+# --- リマインダー関連のコマンドをBotに登録するファイル！
 
 from .daily import setup as setup_reminder
 
+# メインから呼ばれる setup_reminder 関数（Botにコマンドを登録する）
 async def setup_reminder_commands(bot):
-    await bot.add_cog(daily.DailyReminder(bot))
+    await setup_reminder(bot)
