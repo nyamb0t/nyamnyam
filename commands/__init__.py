7a97b00bbffdb13a2d90e4cc72a04ea1c7521f80
nyamb0t <nrnm-a@yahoo.co.jp>
2025-04-28 18:25:59 +0900

    __init__.py を更新

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,7 +4,7 @@
 from . import slash_channel  # 数字転送用のスラッシュコマンド
 from . import slash_vc       # VC設定用のスラッシュコマンド
 from . import slash_show     # 部屋番の設定確認
-from . import slash_debug
+#from . import slash_debug
 
 # Bot起動時に呼ばれるコマンド登録関数
 async def setup_commands(bot):
@@ -12,4 +12,4 @@ async def setup_commands(bot):
     await slash_channel.setup(bot)
     await slash_vc.setup(bot)
     await slash_show.setup(bot)
-    await slash_debug.setup(bot)
+    #await slash_debug.setup(bot)
