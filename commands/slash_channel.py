97c02af0c806627221ad45a11d79bd2436fbdb78
nyamb0t <nrnm-a@yahoo.co.jp>
2025-04-28 18:42:13 +0900

    slash_channel.py を更新

--- commands/slash_channel.py
+++ commands/slash_channel.py
@@ -6,7 +6,7 @@ from discord import app_commands
 from utils.channel_storage import load_guild_data, save_guild_data
 
 # --- チャンネル追加コマンド（/chset）
-@app_commands.command(name="chset", description="部屋番をおくるチャンネルを追加する")
+@app_commands.command(name="number_set", description="部屋番をおくるチャンネルを追加する")
 @app_commands.describe(channel="おくる先のチャンネル")
 async def chset(interaction: discord.Interaction, channel: discord.TextChannel):
     guild_id = interaction.guild.id
@@ -22,7 +22,7 @@ async def chset(interaction: discord.Interaction, channel: discord.TextChannel):
     await interaction.response.send_message(f"{channel.mention} に部屋番送るね♩", ephemeral=True)
 
 # --- チャンネル削除コマンド（/chdelete）
-@app_commands.command(name="chdelete", description="部屋番をおくるチャンネルを解除する")
+@app_commands.command(name="number_delete", description="部屋番をおくるチャンネルを解除する")
 @app_commands.describe(channel="解除したいチャンネル（指定しなければ全部きえる！）")
 async def chdelete(interaction: discord.Interaction, channel: discord.TextChannel = None):
     guild_id = interaction.guild.id
@@ -41,10 +41,10 @@ async def chdelete(interaction: discord.Interaction, channel: discord.TextChanne
 
     data["text_channels"].remove(channel.id)
     save_guild_data(guild_id, data)
-    await interaction.response.send_message(f"{channel.mention} もう送らないからねー", ephemeral=True)
+    await interaction.response.send_message(f"{channel.mention} もう送らないからねーᴖ ̫ᴖ", ephemeral=True)
 
 # --- Botにコマンド登録する setup 関数（__init__.py から呼び出す）
 async def setup(bot: discord.Client):
-    bot.tree.add_command(chset)
-    bot.tree.add_command(chdelete)
+    bot.tree.add_command(number_set)
+    bot.tree.add_command(number_delete)
     # await bot.tree.sync()  # コマンドをDiscordに同期！
